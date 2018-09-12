e6ab6545b0e4b809cb720c4cd2caf2b082cbcec5
wcypersonal <417177466@qq.com>
2018-09-12 20:51:48 +0800

    shimming

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ generate 'package.json'
 #master
 初始化项目，webpack构建依赖图，bundle解决调用板块之间的依赖关系,在dist文件夹下编译出main.js
 配置webpack的配置文件：webpack.config.js
+
+# shimming(feature/shimming)
+（1）去掉不合规矩的全局依赖；希望polyfill浏览器功能以支持更多的用户
+（2）使用ProvidePlugin暴露某个模块中单个导出值,e.g去掉lodash中不需要调用的部分
+（3）细粒度：this普遍指代window，this在CommonJS中指代module.exports
+通过imports-loader等来选择
+将全局变量作为一个普通的模块来导出
+(4) polyfills:不放在主bundle里面
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+// import "babel-polyfill"
 
 function component() {
     var element = document.createElement('div');
 
-    element.innerHTML =  _.join(['Hello','webpack'],' ');
+    element.innerHTML = join(['Hello','webpack'],' ');
+    this.alert('Hmm, this probably isn\'t a great idea...')
 
     return element
 }
 
 document.body.appendChild(component())
+// TO DO polyfills
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => {
+        console.log('We retrieved some data! AND we\'re confident it will work on a variety of browser distributions.')
+        console.log(json)
+    })
+    .catch(error => console.error('Something went wrong when fetching this data: ', error))
