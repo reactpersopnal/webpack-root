0c53f60413e250dbfbf0b01073f7e01c6bdee81a
wcypersonal <417177466@qq.com>
2018-09-12 15:07:23 +0800

    添加css分离插件

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,37 @@
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const extractCSS = new ExtractTextPlugin('stylesheets/[name]-one.css')
+const extractLESS = new ExtractTextPlugin('stylesheets/[name]-two.css')
 
 module.exports = {
     entry: {
         app: "./src/index.js"
     },
+    rules: [
+        {
+            // test: /\.css$/,
+            // use: ExtractTextPlugin.extract({
+            //     fallback: "style-loader",
+            //     use: "css-loader"
+            // })
+            test: /\.css$/,
+            use: extractCSS.extract(['css-loader','postcss-loader'])
+        },
+        {
+            test: /\.less$/i,
+            use: extractLESS.extract(['css-loader','less-loader'])
+        }
+    ],
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title:'Production'
-        })
+        }),
+        // new ExtractTextPlugin("styles.css"),
+        extractCSS,
+        extractLESS
     ],
     output: {
         filename: '[name].bundle.js',
