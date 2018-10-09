82626c34096f9196e0e7abe223c7ddc0d3168d2f
wcypersonal <417177466@qq.com>
2018-10-09 21:38:41 +0800

    尝试webpack代码分离的其他用法

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-// import _ from 'lodash';
-// import printMe from './print.js'
-//
-// function component() {
-//     var element = document.createElement('div');
-//     var btn = document.createElement('button')
-//
-//     element.innerHTML =  _.join(['Hello','webpack测试'],' ');
-//
-//     btn.innerHTML = 'Click me and check the console!';
-//     btn.onclick = printMe
-//
-//     element.appendChild(btn)
-//
-//
-//     return element
-// }
-//
-// document.body.appendChild(component())
+import _ from 'lodash';
+import printMe from './print.js'
+require('./style.css')
+
+function component() {
+    var element = document.createElement('div');
+    var btn = document.createElement('button')
+
+    element.innerHTML =  _.join(['Hello','webpack测试'],' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe
+
+    element.appendChild(btn)
+
+
+    return element
+}
+
+document.body.appendChild(component())
 // function getComponent() {
 //     return import(/* webpackChunkName: "lodash"*/ 'lodash').then(_ => {
 //         var element = document.createElement('div')
@@ -28,15 +29,15 @@
 //     }).catch(error => 'An error occured while loading the component')
 // }
 
-async function getComponent() {
-    var element = document.createElement('div')
-    const _ = await import(/*webpackCunkName: "lodash"*/ 'lodash')
-
-    element.innerHTML = _.join(['Hello','webpack'],'')
-
-    return element
-}
-
-getComponent().then(component => {
-    document.body.appendChild(component)
-})
+// async function getComponent() {
+//     var element = document.createElement('div')
+//     const _ = await import(/*webpackCunkName: "lodash"*/ 'lodash')
+//
+//     element.innerHTML = _.join(['Hello','webpack'],'')
+//
+//     return element
+// }
+//
+// getComponent().then(component => {
+//     document.body.appendChild(component)
+// })
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,70 @@
 const path = require('path')
-// const webpack = require('webpack')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const ManifestPlugin = require('webpack-manifest-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+//创建多个实例
+// const extractCSS = new ExtractTextPlugin('stylesheets/[name]-one.css')
+// const extractLESS = new ExtractTextPlugin('stylesheets/[name]-two.css')
 
 module.exports = {
     entry: {
-        // index: './src/index.js',
-        // another: './src/another-module.js'
-        index: './src/index.js'
+        index: './src/index.js',
+        another: './src/another-module.js'
+        // index: './src/index.js'
+    },
+    module: {
+        rules: [
+            // {
+            //     test: /\.css$/,
+            //     use: extractCSS.extract(['css-loader','postcss-loader'])
+            // },
+            // {
+            //     test: /\.less$/i,
+            //     use: extractLESS.extract(['css-loader','less-loader'])
+            // }
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.bundle\.js$/,
+                use: 'bundle-loader'
+            }
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Code Splitting'
+        }),
+        new BundleAnalyzerPlugin(),
+        // new ExtractTextPlugin("styles.css")
+        // extractCSS,
+        // extractLESS
+        new ExtractTextPlugin({
+            filename:"css/style.css"
         })
     ],
-    // optimization: {
-    //     splitChunks: {
-    //         cacheGroups: {
-    //             commons: {
-    //                 name: "common",
-    //                 chunks: "initial",
-    //                 minChunks: 2
-    //             }
-    //         }
-    //     }
-    // },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    name: "common",
+                    chunks: "initial",
+                    minChunks: 2
+                }
+            }
+        }
+    },
     output: {
         filename: "[name].bundle.js",
-        chunkFilename: "[name].bundle.js",
+        // chunkFilename: "[name].bundle.js",
         path: path.resolve(__dirname,'dist')
     }
 }
